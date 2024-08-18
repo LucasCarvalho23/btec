b469e324db5f51ff49fad76cd37e1fef5b5a289c
LucasCarvalho23 <lucasilva1990@hotmail.com>
2024-08-18 11:00:43 -0300

    funcionando com senha md5

--- App/Models/Login.php
+++ App/Models/Login.php
@@ -19,8 +19,8 @@
         public function validarLogin() {
             $query = "SELECT id, login, senha FROM cadastro WHERE login = :login AND senha = :senha";
             $stmt = $this->db->prepare($query);
-            $stmt->bindValue(":login", $_POST['login']);
-            $stmt->bindValue(":senha", md5($_POST['senha']));
+            $stmt->bindValue(":login", $_POST['login'], \PDO::PARAM_STR);
+            $stmt->bindValue(":senha", md5($_POST['senha']), \PDO::PARAM_STR);
             $stmt->execute();
             $usuario = $stmt->fetch(\PDO::FETCH_ASSOC);
             if (isset($usuario['login']) && isset($usuario['senha'])) {
@@ -32,7 +32,7 @@
         }
 
         public function inserirLogin() {
-            $query = "INSERT INTO cadastro(cpf, login, senha) VALUES ('12160073733', :login, :senha)";
+            $query = "INSERT INTO cadastro(cpf, login, senha) VALUES ('11111111111', :login, :senha)";
             $stmt = $this->db->prepare($query);
             $stmt->bindValue(':login', $this->__get('login'));
             $stmt->bindValue(':senha', $this->__get('senha'));
